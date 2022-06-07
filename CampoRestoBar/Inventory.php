5201aef1fe5408b56d503984252412191fda5907
Koswii <djmaglaqui@gmail.com>
2022-06-07 14:18:14 +0800

    tite

--- CampoRestoBar/Inventory.php
+++ CampoRestoBar/Inventory.php
@@ -31,7 +31,7 @@
                         <span>₱</span><input type="text" id="ingNewPrice" class="ingNewPrice" name="ingNewPrice" value="0000.00">
                     </div>
                     <div>
-                        <input type="button" id="addIngSubmit" name="submit" value="Add Ingredient">
+                        <button id="addIngSubmit">Add Ingredient</button>
                     </div>
                 </div>
             </div>
@@ -44,7 +44,7 @@
             <div class="ingRestock">
                 <h1>Restock Ingredient</h1>
                 <div class="ingRes">
-                    <div id="ingResName">
+                    <div id="ingResName" class="ingResName">
                         <div>
                             <label for="ingredientName">Restock</label>
                             <select name="ingredientName" id="ingredientName">
@@ -69,8 +69,18 @@
                             </Select>
                         </div>
                     </div>
+                    <div>
+                        <label for="">Price</label>
+                        <span>₱</span><input type="number" id="resPrice" class="resPrice" name="resPrice" value="0000.00">
+                    </div>
+                    <div>
+                        <button>Restock Ingredient</button>
+                    </div>
                 </div>
             </div>
         </form>
+        <div class="invModalButtons">
+            
+        </div>
     </div>
 </section>
