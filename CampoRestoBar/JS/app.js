612856f56290eaea48bbf97b379e75417ae2332d
Koswii <djmaglaqui@gmail.com>
2022-06-08 03:41:30 +0800

    update

--- CampoRestoBar/JS/app.js
+++ CampoRestoBar/JS/app.js
@@ -1,3 +1,4 @@
+
 function searchFilter() {  
     var input, filter, table, tr, td, i, txtValue, alert;
     input = document.getElementById("searchIng");
@@ -189,3 +190,25 @@ function enableRestock(){
         }
 }
 
+// Disable Inspect element
+// document.addEventListener('contextmenu',(e)=>{
+//     e.preventDefault();
+//   }
+//   );
+//   document.onkeydown = function(e) {
+//   if(event.keyCode == 123) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+//      return false;
+//   }
+//   if(e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+//      return false;
+//   }
+// }
